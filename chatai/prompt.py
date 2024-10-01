bf47ea37880fda76a9f47c7f2e2a728a6582a901
bsharchilev <bsharchilev@gmail.com>
2024-10-01 20:11:52 +0100

    Update prompt.py

---
+++ chatai/prompt.py
@@ -0,0 +1,29 @@
+from typing import Dict, List
+from types import ChatMessage, CompletionMessage
+
+
+USERNAME_TO_DISPLAY_NAME = {
+    "@sanchar13": "Александр Соболь",
+    "@yolo322": "Андрей Гайбун",
+    "@ushanov_dmitriy": "Дмитрий Ушанов",
+    "@vgrigorash": "Виталий Григораш",
+    "@ZEMUSHKA": "Андрей Зимовнов",
+    "@svyatslv": "Святослав Юшин",
+    "@korolpiratov": "Евгения Пономаренко",
+    "@bsharchilev": "Борис Шарчилев",
+}
+
+class Prompt:
+    def __init__(self, prompt_file_path: str):
+        self.prompt_file_path = prompt_file_path
+        
+    def generate(self, messages: List[ChatMessage]) -> List[CompletionMessage]:
+        with open(self.prompt_file_path, 'r') as f:
+            system_prompt = f.read()
+        result = [{"role": "system", "content": system_prompt}]
+        for message in messages:
+            user = USERNAME_TO_DISPLAY_NAME[message.username]
+            content = f"(от: {user}) {message.text}"
+            result.append({"role": "user", "content": content})
+        return result
+        
