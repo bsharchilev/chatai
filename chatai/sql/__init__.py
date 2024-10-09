a0bc7e15c47201ec2ab3a6ae0079dc2ddd35379c
Boris Sharchilev <sharch@meta.com>
2024-10-09 23:46:05 +0100

    Remove comments

--- chatai/sql/__init__.py
+++ chatai/sql/__init__.py
@@ -3,8 +3,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-# Define the engine (adjust the connection string to your database)
 engine = create_engine(os.getenv("DATABASE_URI"))
 
-# Create a configured "Session" class
 Session = sessionmaker(bind=engine)
