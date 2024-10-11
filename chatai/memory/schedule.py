aec0611930d1bf1ec352640fc3bda184d5b9d725
Boris Sharchilev <sharch@meta.com>
2024-10-11 11:30:56 +0100

    debug cron job

--- chatai/memory/schedule.py
+++ chatai/memory/schedule.py
@@ -1,17 +1,12 @@
 from crontab import CronTab
 
 
-_cron_command = """
-cd /home/bsharchilev/chatai &&
-source chatai-env/bin/activate &&
-source /home/bsharchilev/.env &&
-python -m chatai.memory.extract'
-"""
+_cron_command = "cd /home/bsharchilev/chatai && source chatai-env/bin/activate && source /home/bsharchilev/.env && python -m chatai.memory.extract"
 
 def create_cron_job(cron: CronTab):
     if not any(job for job in cron if job.command == _cron_command):
         job = cron.new(command=_cron_command)
-        job.setall('30 11 * * *')
+        job.setall('32 11 * * *')
         cron.write()
         print("Cron job created")
     else:
